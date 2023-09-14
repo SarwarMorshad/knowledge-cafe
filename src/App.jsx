55ce1c779993baee3ee30db28912c8f38a99c237
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-14 21:49:17 +0200

    Adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <main className="md:flex container mx-auto pl-4 pt-4 bg-[#ffecd1]">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </main>
     </>
   );
 }
