ff87f697490cb87c768ee94d00a9145bcf15ec54
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-14 20:41:56 +0200

    Adding Resourses: Fake json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-7xl bg-orange-300">Knowledge Cafe</h1>
-      <button className="btn btn-accent">Accent</button>
+      <Header></Header>
     </>
   );
 }
