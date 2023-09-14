f78efae9b498bc1151941f4bf31228035d175513
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-14 20:20:44 +0200

    Setting up project: React, tailwind, daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl bg-orange-300">Knowledge Cafe</h1>
+      <button className="btn btn-accent">Accent</button>
+    </>
+  );
+}
+
+export default App;
